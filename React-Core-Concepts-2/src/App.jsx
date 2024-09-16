e046b939798f47ff153a35d06ec0ae45e714d1c0
Md. Shakil <shakil.eub.cse@gmail.com>
2024-09-16 09:40:43 +0600

    Counter

--- React-Core-Concepts-2/src/App.jsx
+++ React-Core-Concepts-2/src/App.jsx
@@ -1,4 +1,6 @@
 import './App.css'
+import Counter from './Counter';
+
 
 function App() {
 
@@ -18,6 +20,8 @@ function App() {
   return (
     <>
       <h3>React Core Concepts 2</h3>
+      <Counter></Counter>
+
       <button onClick={handleClick}>Click Me</button>
       <button onClick={handleClick2}>Click2</button>
       <button onClick={() => { alert('Third clicked')}}>Third</button>
