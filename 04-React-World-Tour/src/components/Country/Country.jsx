a41af40e9f92c8f95d5584956a19ec752ca0c7f7
Md. Shakil <shakil.eub.cse@gmail.com>
2024-10-13 02:11:44 +0600

    Toggle state based on click

--- 04-React-World-Tour/src/components/Country/Country.jsx
+++ 04-React-World-Tour/src/components/Country/Country.jsx
@@ -1,13 +1,23 @@
+import { useState } from 'react';
 import './Country.css';
 const Country = ({ country }) => {
     console.log(country)
     const { name, flags, population, area } = country;
+
+    const [visited, setVisited] = useState(false);
+
+    const handleVisited = () =>{
+        setVisited(!visited);
+    }
+
     return (
         <div className='country'>
             <img src={ flags.png } alt="" />
             <h3>Name: { name?.common }</h3>
             <p>Population: {population}</p>
             <p>Area: {area}</p>
+            <button onClick={handleVisited}>{visited ? 'Visited' : 'Going'}</button>
+            {visited && 'I have visited this country'}
         </div>
     );
 };
