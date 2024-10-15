336abea4543947eac6e1e30994c81f9d0f28f947
Md. Shakil <shakil.eub.cse@gmail.com>
2024-10-15 10:52:04 +0600

    blog data view as blog component

--- 07-Knowledge-Cafe-Project/src/components/Blog/Blog.jsx
+++ 07-Knowledge-Cafe-Project/src/components/Blog/Blog.jsx
@@ -1,10 +1,29 @@
 import PropTypes from "prop-types";
 
 const Blog = ({ blog }) => {
-    console.log(blog);
+    const {cover_img, title, author, author_img, posted_date, reading_time, hashtags} = blog;
+    
     return (
         <div>
-            
+            <img src={cover_img} alt={`cover picture of the title ${title}`} />
+            <div className="flex justify-between">
+                <div className="flex"> 
+                    <img className="w-14 rounded-full" src={author_img} alt="" />
+                    <div className="ml-6">
+                        <h3 className="text-2xl">{author}</h3>
+                        <p>{posted_date}</p>
+                    </div>
+                </div>
+                <div>
+                    <span>{reading_time} min read</span>
+                </div>
+            </div>
+            <h2 className="text-4xl">{title}</h2>
+            <p>
+                {
+                    hashtags.map((hash, idx) => <span key={idx}><a href="">{hash}</a></span>)
+                }
+            </p>
         </div>
     );
 };
