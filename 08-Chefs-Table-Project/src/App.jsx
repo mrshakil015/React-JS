c2cae16dcaab8f60627b20589c317390c4bad870
Md. Shakil <shakil.eub.cse@gmail.com>
2024-10-15 22:15:56 +0600

    Configure project

---
+++ 08-Chefs-Table-Project/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Header from './components/Header/Header'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-4xl'>Chefs Table</h1>
+      <Header></Header>
+    </>
+  )
+}
+
+export default App
