3fb8834e205454085e0238c80f90361c9c6dbd24
Md. Shakil <shakil.eub.cse@gmail.com>
2024-10-15 11:14:47 +0600

    layout styles

--- 07-Knowledge-Cafe-Project/src/components/Blog/Blog.jsx
+++ 07-Knowledge-Cafe-Project/src/components/Blog/Blog.jsx
@@ -1,25 +1,27 @@
 import PropTypes from "prop-types";
+import { IoBookmarksOutline  } from "react-icons/io5";
 
 const Blog = ({ blog }) => {
     const {cover_img, title, author, author_img, posted_date, reading_time, hashtags} = blog;
     
     return (
-        <div>
-            <img src={cover_img} alt={`cover picture of the title ${title}`} />
-            <div className="flex justify-between">
+        <div className="mb-20">
+            <img className="w-full h-[450px] object-cover mb-8 rounded-md" src={cover_img} alt={`cover picture of the title ${title}`} />
+            <div className="flex justify-between mb-4">
                 <div className="flex"> 
                     <img className="w-14 rounded-full" src={author_img} alt="" />
                     <div className="ml-6">
-                        <h3 className="text-2xl">{author}</h3>
+                        <h3 className="text-2xl font-bold">{author}</h3>
                         <p>{posted_date}</p>
                     </div>
                 </div>
-                <div>
+                <div className="space-x-2">
                     <span>{reading_time} min read</span>
+                    <button><IoBookmarksOutline  /></button>
                 </div>
             </div>
-            <h2 className="text-4xl">{title}</h2>
-            <p>
+            <h2 className="text-4xl font-bold mb-4">{title}</h2>
+            <p className="space-x-3">
                 {
                     hashtags.map((hash, idx) => <span key={idx}><a href="">{hash}</a></span>)
                 }
