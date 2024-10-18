4e34e39845dc3a7f7cd16e1229a4c04d51108156
Md. Shakil <shakil.eub.cse@gmail.com>
2024-10-18 23:13:45 +0600

    Home page layout

---
+++ 13-Career-Hub-Website/src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -0,0 +1,12 @@
+const FeaturedJobs = () => {
+    return (
+        <div className="text-center">
+            <h2 className="text-5xl">Featured Jobs</h2>
+            <p>
+                Explore thousands of job opportunities with all the information you need. Its your future
+            </p>
+        </div>
+    );
+};
+
+export default FeaturedJobs;
