78afe8074f8e0d2967c0053bb13b6c96b9e7db90
Md. Shakil <shakil.eub.cse@gmail.com>
2024-10-22 23:18:58 +0600

    setup login and register components

---
+++ 17-Auth-Integration-Private-Auth/src/components/Register/Register.jsx
@@ -0,0 +1,52 @@
+import { Link } from "react-router-dom";
+
+const Register = () => {
+
+    const handleRegister = e =>{
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log(email, password)
+    }
+
+    return (
+        <div className="hero bg-base-200 min-h-screen">
+            <div className="hero-content flex-col">
+                <div className="text-center">
+                    <h1 className="text-5xl font-bold">Register now!</h1>
+                </div>
+                <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                    <form onSubmit={handleRegister} className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input type="text" name="name" placeholder="Your Name" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" name="email" placeholder="Your Email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" name="password" placeholder="Your Password" className="input input-bordered" required />
+                            <label className="label">
+                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Login</button>
+                        </div>
+                        <p className="flex items-center">Already have an account? Go to <Link to="/login"><button className="btn btn-link">Login</button></Link></p>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Register;
