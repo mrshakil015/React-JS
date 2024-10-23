0faf93b96b8c543543407177920ea14d5515090a
Md. Shakil <shakil.eub.cse@gmail.com>
2024-10-23 22:19:44 +0600

    routes and root setup

--- 18-React-Dragon-News-Auth/src/main.jsx
+++ 18-React-Dragon-News-Auth/src/main.jsx
@@ -1,22 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { RouterProvider } from 'react-router-dom'
+import routes from './routes/Route'
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-
-const router = createBrowserRouter([
-  {
-      path: "/",
-      element: <div>Hello world!</div>,
-  },
-]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router}></RouterProvider>
+    <RouterProvider router={routes}></RouterProvider>
   </StrictMode>,
 )
