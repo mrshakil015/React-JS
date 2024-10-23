d76f9008a387fcf0e57320e7580053a1813b57f3
Md. Shakil <shakil.eub.cse@gmail.com>
2024-10-23 23:32:03 +0600

    Homepage layout and date time formatting

--- 18-React-Dragon-News-Auth/src/pages/Home/Home.jsx
+++ 18-React-Dragon-News-Auth/src/pages/Home/Home.jsx
@@ -1,7 +1,24 @@
+import Header from "../Shared/Header/Header";
+import LeftSideNav from "../Shared/LeftSideNav/LeftSideNav";
+import Navbar from "../Shared/Navbar/Navbar";
+import RightSideNav from "../Shared/RightSideNav/RightSideNav";
+
 const Home = () => {
     return (
         <div>
-            <h2 className="text-3xl">This is home</h2>            
+            <Header></Header>
+            <Navbar></Navbar>
+            <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
+                <div className="border">
+                    <LeftSideNav></LeftSideNav>
+                </div>
+                <div className="md:col-span-2 border">
+                    <h2 className="text-4xl">News Comming ....</h2>
+                </div>
+                <div className="border">
+                    <RightSideNav></RightSideNav>
+                </div>
+            </div>
         </div>
     );
 };
