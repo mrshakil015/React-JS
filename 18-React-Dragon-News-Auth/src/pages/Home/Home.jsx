d0f9e6647324bdb03eb09e27cd0d00978866599e
Md. Shakil <shakil.eub.cse@gmail.com>
2024-10-24 01:39:19 +0600

    News Section include

--- 18-React-Dragon-News-Auth/src/pages/Home/Home.jsx
+++ 18-React-Dragon-News-Auth/src/pages/Home/Home.jsx
@@ -1,10 +1,18 @@
+import { useEffect, useState } from "react";
 import Header from "../Shared/Header/Header";
 import LeftSideNav from "../Shared/LeftSideNav/LeftSideNav";
 import Navbar from "../Shared/Navbar/Navbar";
 import RightSideNav from "../Shared/RightSideNav/RightSideNav";
 import BreakingNews from "./BreakingNews";
+import News from "./News";
 
 const Home = () => {
+    const [newsData, setNewsData] = useState([]);
+    useEffect(() => {
+        fetch('news.json')
+            .then(res => res.json())
+            .then(data => setNewsData(data))
+    }, [])
     return (
         <div>
             <Header></Header>
@@ -14,9 +22,18 @@ const Home = () => {
                 <div className="border">
                     <LeftSideNav></LeftSideNav>
                 </div>
-                <div className="md:col-span-2 border">
-                    <h2 className="text-4xl">News Comming ....</h2>
+
+                {/* main news section */}
+                <div className="md:col-span-2">
+                <h2>Dragon News Home: {newsData.length}</h2>
+                <div>
+                    {
+                        newsData.map(news => <News key={news.id} news={news}></News>)
+                    }
                 </div>
+                </div>
+
+
                 <div className="border">
                     <RightSideNav></RightSideNav>
                 </div>
---
+++ 18-React-Dragon-News-Auth/src/pages/Home/News.jsx
@@ -0,0 +1,49 @@
+import { FaBookmark, FaEye, FaShareAlt, FaStar } from "react-icons/fa";
+
+const News = ({ news }) => {
+    const { id, category_id, title, details, total_view, rating, author, image_url } = news
+    const { name, published_date, img } = author
+    return (
+        <div className="my-5 border rounded-lg">
+            <div className="flex flex-row items-center justify-between gap-4 bg-[#f3f3f3]  p-4 rounded-t-lg">
+                <div className="flex items-center gap-4">
+                    <img className="w-10 rounded-full" src={img} alt="" />
+                    <div>
+                        <h2 className="font-bold">{name}</h2>
+                        <p className="text-[#706f6f]">{published_date}</p>
+                    </div>
+                </div>
+                <div className="flex items-center gap-2">
+                    <FaBookmark />
+                    <FaShareAlt />
+                </div>
+            </div>
+            <div className="p-5">
+                <h2 className="text-xl font-bold ">{title}</h2>
+                <img className="my-5" src={image_url} alt="" />
+                <p>{details}</p>
+            </div>
+            <hr />
+
+            <div className="flex justify-between p-4">
+                <div className="flex items-center gap-3">
+                    <div className="flex gap-2 items-center">
+                        <FaStar className="text-orange-500"></FaStar>
+                        <FaStar className="text-orange-500"></FaStar>
+                        <FaStar className="text-orange-500"></FaStar>
+                        <FaStar className="text-orange-500"></FaStar>
+                        <FaStar className="text-orange-500"></FaStar>
+                    </div>
+                    <p>{rating.number}</p>
+                </div>
+                <div className="flex items-center gap-2">
+                    <FaEye></FaEye>
+                    <p>{total_view}</p>
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default News;
